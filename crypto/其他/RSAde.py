8208e5a14eeb40c7bb1ca11357d0ba9549ff44c0
sheldonl3 <tssheldonpotter@gmail.com>
2024-01-20 07:18:27 -0500

    rsa练习

---
+++ crypto/其他/RSAde.py
@@ -0,0 +1,17 @@
+import gmpy2
+import rsa
+#https://buuoj.cn/challenges#RSA
+
+e = 65537
+n = 86934482296048119190666062003494800588905656017203025617216654058378322103517
+p = 285960468890451637935629440372639283459
+q = 304008741604601924494328155975272418463
+
+phin = (q-1)*(p-1)
+d = gmpy2.invert(e, phin)
+
+key = rsa.PrivateKey(n, e, int(d), p, q)
+
+with open("flag.enc", "rb+") as f:
+    f = f.read()
+    print(rsa.decrypt(f, key))
