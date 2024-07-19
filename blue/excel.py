99f0aec319738b28678b544f4cd6ac50eebd2ff1
sheldonl3 <44045052+sheldonl3@users.noreply.github.com>
2024-07-20 02:03:38 +0800

    excel只读取一次文件

--- blue/excel.py
+++ blue/excel.py
@@ -3,51 +3,46 @@ import os
 import pandas as pd
 
 df = pd.DataFrame([])
-df.to_excel(r'端口统计.xlsx')
 
 
-# 查找跳过多少行才是端口数据
 def find_skip_row(file):
-    skiprows = 0
     try:
         xls = pd.read_excel(file, sheet_name="其它信息", header=None)
-    except Exception as e:  # 使用具体的异常类型可能更好，但这里为了简单起见使用Exception
-        print(f"v1 {file} has no data: {e}")
+    except Exception as e:
+        print(f"{file} has no data: {e}")
+        return None
     else:
         if xls.empty:
-            print(file + "v2 has no data:empty")
+            print(f"{file} has no data: empty")
+            return None
         target_value = '远程端口信息'
-        skiprows = xls.index[xls.iloc[:, 0] == target_value].item()
-        print('skiprows is ' + str(skiprows))
-    return skiprows
+        skiprows = xls.index[xls.iloc[:, 0] == target_value]
+        if skiprows.empty:
+            print(f"{file} has no target value: {target_value}")
+            return None
+        skiprows = skiprows.item()
+        xls = xls.iloc[skiprows + 2:]  # 跳过前面不要的行
+        xls = xls.iloc[:, 1:-1]  # 删除第一列和最后一列开放状态
+        first_empty_row = xls.apply(lambda row: row.isna().all(), axis=1).idxmax()  # 上面做完之后，找到第一个全为空的行，截断，就只剩下端口数据了
+        if pd.notna(first_empty_row) and first_empty_row > 0:
+            xls = xls.iloc[:(first_empty_row - skiprows - 2)]  # 索引在xls.iloc[skiprows+2:]之后没有变，所以要多减
+    return xls
 
 
 # 先将文件写入一个列表，稍后再处理
-excel_files = []
-for file in os.listdir("."):
-    if file.endswith('.xlsx') or file.endswith('.xls'):  # 确保文件是Excel格式
-        excel_files.append(file)
+excel_files = [file for file in os.listdir(".") if file.endswith('.xlsx') or file.endswith('.xls')]
 
 for file in excel_files:
     print(file + '_____')
     try:
-        xls = pd.read_excel(file, sheet_name="其它信息", header=1, usecols=['端口', '协议', '服务'],
-                            skiprows=find_skip_row(file))
-    except Exception as e:  # 使用具体的异常类型可能更好，但这里为了简单起见使用Exception
-        print(f"{file} has no data: {e}")
+        xls = find_skip_row(file)
+        if xls is not None:
+            xls = xls.assign(ip=file[:-4])#加入ip信息
+            df = pd.concat([df, xls], ignore_index=True)
+    except ValueError as e:
+        print(f"Error processing file {file}: {e}")
         continue
-    else:
-        if xls.empty:
-            print(file + " has no data:empty")
-            continue
 
-        # 查找第一个全为空值的行（包括索引为0的标题行）
-        first_empty_row = xls.iloc[0:].apply(lambda row: row.isna().all(), axis=1).idxmax()
-        # 如果找到了全为空值的行，则删除该行及其之后的所有行
-        if pd.notna(first_empty_row):
-            xls = xls.iloc[:first_empty_row]
-        xls = xls.assign(ip=file[:-4])
-        df = pd.concat([df, xls], ignore_index=True)
-
+df = df.rename(columns={1: '端口', 2: '协议', 3: '服务'})
 print(df)
 df.to_excel('端口统计.xlsx', index=False)
